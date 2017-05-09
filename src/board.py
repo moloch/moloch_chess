820aafb9df95f4f68e598ac44902a796cc05233b
Dario Coco <dario.coco@xpeppers.com>
2017-05-09 16:08:44 +0200

    Added board coordinates translation methods

--- src/board.py
+++ src/board.py
@@ -1,7 +1,18 @@
 class Board:
 
+    def __init__(self):
+        self.squares = [[Square() for i in range(0, 8)] for j in range(0, 8)]
+
     def get_square(self, coordinates):
-        return Square()
+        x = self.translate_x(coordinates[0])
+        y = self.translate_y(coordinates[1])
+        return self.squares[x][7]
+
+    def translate_x(self, x):
+        return ord(x) - 97
+
+    def translate_y(self, y):
+        return int(8 - int(y))
 
 
 class Square:
