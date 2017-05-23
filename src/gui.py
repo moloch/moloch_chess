ba304d681abb1865c9b7753a6ecbde7c6fc06c0f
Dario Coco <dario.coco@xpeppers.com>
2017-05-23 20:12:06 +0200

    First implementation of Board in GUI

--- src/gui.py
+++ src/gui.py
@@ -6,12 +6,33 @@ from PyQt5.QtWidgets import (QApplication, QGraphicsView, QGraphicsScene, QGraph
                              QGridLayout, QVBoxLayout, QHBoxLayout,
                              QLabel, QLineEdit, QPushButton, QGraphicsPixmapItem, QGraphicsRectItem)
 
+from board import Board
+
+
+def generate_board(scene):
+    board = Board()
+    for line in board.squares:
+        for square in line:
+            square_graphic_item = SquareGraphicsItem()
+            square_graphic_item.setRect(QRectF(square.x*70, square.y*70, 70, 70))
+            if square.is_black():
+                square_graphic_item.setBrush(QBrush(QColor("grey")))
+            else:
+                square_graphic_item.setBrush(QBrush(QColor("white")))
+            scene.addItem(square_graphic_item)
+
+
+class SquareGraphicsItem(QGraphicsRectItem):
+    def __init__(self):
+        super(SquareGraphicsItem, self).__init__()
+        self.setAcceptDrops(True)
 
 class BoardGraphicsItem(QGraphicsRectItem):
     def __init__(self, scene):
         super(BoardGraphicsItem, self).__init__()
         self.setAcceptDrops(True)
         self.scene = scene
+        generate_board(scene)
 
     def dragEnterEvent(self, event):
         print("Enter Drag")
@@ -58,14 +79,13 @@ class MainWindow(QGraphicsView):
         super(MainWindow, self).__init__()
         scene = QGraphicsScene(self)
         rectangle = BoardGraphicsItem(scene)
-        rectangle.setRect(QRectF(0, 0, 300, 300))
+        rectangle.setRect(QRectF(0, 0, 600, 600))
         scene.addItem(rectangle)
         queen_pixmap = QPixmap("img/w_Q.png")
         queen_pixmap = queen_pixmap.scaledToHeight(64, Qt.SmoothTransformation)
         queen = PieceGraphicsPixmapItem(queen_pixmap)
-        queen.setOffset(100, 100)
         scene.addItem(queen)
-        scene.setSceneRect(0, 0, 300, 300)
+        scene.setSceneRect(0, 0, 600, 600)
         self.setScene(scene)
         self.setCacheMode(QGraphicsView.CacheBackground)
         self.setWindowTitle("Moloch Chess")
