09925090e145dd4d848f61f632bd2ac6bb73a8c1
Dario Coco <dario.coco@xpeppers.com>
2017-05-23 17:43:32 +0200

    Drag and drop Queen experiment

--- src/gui.py
+++ src/gui.py
@@ -1,68 +1,61 @@
 #!/usr/bin/env python
 
-from PyQt5.QtCore import (QLineF, QPointF, QRectF, Qt)
-from PyQt5.QtGui import (QBrush, QColor, QPainter, QPixmap)
+from PyQt5.QtCore import (QLineF, QPointF, QRectF, Qt, QMimeData, QByteArray, QDataStream, QPoint, QIODevice, QBuffer)
+from PyQt5.QtGui import (QBrush, QColor, QPainter, QPixmap, QDrag)
 from PyQt5.QtWidgets import (QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem,
                              QGridLayout, QVBoxLayout, QHBoxLayout,
-                             QLabel, QLineEdit, QPushButton, QGraphicsPixmapItem)
+                             QLabel, QLineEdit, QPushButton, QGraphicsPixmapItem, QGraphicsRectItem)
 
-class TicTacToe(QGraphicsItem):
+
+class BoardGraphicsItem(QGraphicsRectItem):
     def __init__(self):
-        super(TicTacToe, self).__init__()
-        self.board = [[-1, -1, -1],[-1, -1, -1], [-1, -1, -1]]
-        self.O = 0
-        self.X = 1
-        self.turn = self.O
+        super(BoardGraphicsItem, self).__init__()
 
-    def reset(self):
-        for y in range(3):
-            for x in range(3):
-                self.board[y][x] = -1
-        self.turn = self.O
-        self.update()
 
-    def select(self, x, y):
-        if x < 0 or y < 0 or x >= 3 or y >= 3:
-            return
-        if self.board[y][x] == -1:
-            self.board[y][x] = self.turn
-            self.turn = 1 - self.turn
-
-    def paint(self, painter, option, widget):
-        painter.setPen(Qt.black)
-        painter.drawLine(0,100,300,100)
-        painter.drawLine(0,200,300,200)
-        painter.drawLine(100,0,100,300)
-        painter.drawLine(200,0,200,300)
-
-        for y in range(3):
-            for x in range(3):
-                if self.board[y][x] == self.O:
-                    painter.setPen(Qt.red)
-                    painter.drawEllipse(QPointF(50+x*100, 50+y*100), 30, 30)
-                elif self.board[y][x] == self.X:
-                    painter.setPen(Qt.blue)
-                    painter.drawLine(20+x*100, 20+y*100, 80+x*100, 80+y*100)
-                    painter.drawLine(20+x*100, 80+y*100, 80+x*100, 20+y*100)
-
-    def boundingRect(self):
-        return QRectF(0,0,300,300)
+class PieceGraphicsPixmapItem(QGraphicsPixmapItem):
+    def __init__(self, pixmap):
+        super().__init__(pixmap)
 
     def mousePressEvent(self, event):
-        pos = event.pos()
-        self.select(int(pos.x()/100), int(pos.y()/100))
-        self.update()
-        super(TicTacToe, self).mousePressEvent(event)
+        print("Press")
+
+    def mouseMoveEvent(self, event):
+        itemData = QByteArray()
+        buffer = QBuffer(itemData)
+        buffer.open(QIODevice.WriteOnly)
+        self.pixmap().save(buffer)
+        mimeData = QMimeData()
+        mimeData.setData('application/x-dnditemdata', itemData)
+
+        drag = QDrag(event.widget())
+        drag.setMimeData(mimeData)
+        drag.setPixmap(self.pixmap())
+        x = self.pos().toPoint().x() + self.pixmap().width() / 2
+        y = self.pos().toPoint().y() + self.pixmap().height() / 2
+        drag.setHotSpot(QPoint(x, y))
+        drag.exec_()
+
+
+    def dragEnterEvent(self, event):
+        print("Enter Drag")
+
+    def dragLeaveEvent(self, event):
+        print("Leave Drag")
+
+    def dropEvent(self, event):
+        print("Drop")
+
 
 class MainWindow(QGraphicsView):
     def __init__(self):
         super(MainWindow, self).__init__()
         scene = QGraphicsScene(self)
-        self.tic_tac_toe = TicTacToe()
-        #scene.addItem(self.tic_tac_toe)
+        scene.addItem(BoardGraphicsItem())
         queen_pixmap = QPixmap("img/w_Q.png")
-        queen = queen_pixmap.scaledToHeight(64, Qt.SmoothTransformation)
-        scene.addItem(QGraphicsPixmapItem(queen))
+        queen_pixmap = queen_pixmap.scaledToHeight(64, Qt.SmoothTransformation)
+        queen = PieceGraphicsPixmapItem(queen_pixmap)
+        queen.setOffset(100,100)
+        scene.addItem(queen)
         #Usare QGraphicsPixmapItem per i vari pezzi, la scacchiera sara' lo sfondo QgraphicsView
         scene.setSceneRect(0, 0, 300, 300)
         self.setScene(scene)
